886accfc64f5cd685512045fd2ee5050db2f9ae1
mynorx <mnrdavid@gmail.com>
2014-08-26 19:14:55 -0600

    Update 20140826055555_create_bills.rb

--- db/migrate/20140826055555_create_bills.rb
+++ db/migrate/20140826055555_create_bills.rb
@@ -1,12 +1,8 @@
 class CreateBills < ActiveRecord::Migration
   def self.up
-    create_table :bills do |t|
-
-      t.timestamps
-    end
+    
   end
 
   def self.down
-    drop_table :bills
   end
 end
